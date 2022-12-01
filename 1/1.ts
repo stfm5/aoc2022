66f1ca54bcc8b74f2c561edd3008266d4afde71d
stfm5 <teodorsimonovskii@gmail.com>
2022-12-01 06:08:37 +0100

    day1

--- 1/1.ts
+++ 1/1.ts
@@ -4,19 +4,34 @@ type Input = any;
 
 const parseData = (): Input => {
     console.time("parseData")
+		const elves: number[]= []
+		const input = readInput(1, runSample).split("\n");
+		let calories = 0;
+		input.forEach((line) => {
+			if(line.length === 0) {
+				elves.push(calories)
+				calories = 0;
+			} else {
+				calories += parseInt(line);
+			}
+		})	
+		
     console.timeEnd("parseData")
+		console.log(elves)
+		return elves
 }
 
 const sol1 = (input: Input) => {
 	console.time("sol1")
 	console.timeEnd("sol1")
-	return 1
+	return input.sort((a,b) => b-a)[0];
 }
 
 const sol2 = (input: Input) => {
 	console.time("sol2")
+	const calories = input.sort((a,b) => b-a);
 	console.timeEnd("sol2")
-	return 2;
+	return calories[0] + calories[1] + calories[2];
 }
 
 const input = parseData();
