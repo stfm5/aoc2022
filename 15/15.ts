285cd5f30627f29f89cfdbee65a2d83b367f5880
stfm5 <74566576+stfm5@users.noreply.github.com>
2022-12-15 08:51:09 +0100

    Update 15.ts

--- 15/15.ts
+++ 15/15.ts
@@ -1,12 +1,6 @@
 import readInput from "../input.helper";
 const runSample = false; 
-type Input = {
-  signals: Signal[]
-  maxX: number,
-  maxY: number,
-  minX: number,
-  minY: number,
-};
+type Input = Signal[]
 type Signal = {
   sX: number
   sY: number,
@@ -18,10 +12,6 @@ type Signal = {
 const parseData = (): Input => {
   console.time("parseData")
   const lineRegex = new RegExp(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/);
-  let maxX = Number.MIN_SAFE_INTEGER;
-  let minX = Number.MAX_SAFE_INTEGER;
-  let maxY = Number.MIN_SAFE_INTEGER;
-  let minY = Number.MAX_SAFE_INTEGER;
 
   const signals = readInput(15,runSample).split("\n").map((line) => {
     let [_,sY, sX, bY, bX] = line.match(lineRegex);
@@ -32,33 +22,27 @@ const parseData = (): Input => {
       bY: parseInt(bY),
       mDistance: 0
     }
-    if(signal.sX > maxX || signal.bX > maxX) {
-      maxX = signal.sX >= signal.bX ? signal.sX : signal.bX
-    }
-    if(signal.sX < minX || signal.bX < minX) {
-      minX = signal.sX <= signal.bX ? signal.sX : signal.bX
-    }
-
-    if(signal.sY > maxY || signal.bY > maxY) {
-      maxY = signal.sY >= signal.bY ? signal.sY : signal.bY
-    }
-    if(signal.sY < minY || signal.bY < minY) {
-      minY = signal.sY <= signal.bY ? signal.sY : signal.bY
-    }
 
     signal.mDistance = getDistance(sX, sY, bX, bY);
 
     return signal
   })
-  const input = {
-    signals,
-    maxX,
-    minX,
-    maxY,
-    minY
-  }
+  const input = signals
   console.timeEnd("parseData")
   return input;
+}
+
+const sol1 = (input: Input) => {
+  console.time("sol1")
+  let goal = 20
+  let positions = getPositions(input, goal);
+  positions = positions.filter((p) => {
+    return !input.find((signal) => {
+      return (p === signal.sY && goal === signal.sX) || (p === signal.bY && goal === signal.bX)
+    })
+  })
+  console.timeEnd("sol1")
+  return positions.length
 }
 
 const sol2 = (input: Input) => {
@@ -70,8 +54,8 @@ const sol2 = (input: Input) => {
   for(let i = 0; i <= goal; i++) {
     for(let j = 0; j <= goal; j++) {
       let found = true;
-      for(let c = 0; c < input.signals.length;c++) {
-        let signal = input.signals[c]
+      for(let c = 0; c < input.length;c++) {
+        let signal = input[c]
         let nDistance = getDistance(signal.sX,signal.sY,j,i);
         if(nDistance <= signal.mDistance) {
           found = false
@@ -92,21 +76,9 @@ const sol2 = (input: Input) => {
   return val
 }
 
-const sol1 = (input: Input) => {
-  console.time("sol1")
-  let goal = 20
-  let positions = getPositions(input, goal);
-  positions = positions.filter((p) => {
-    return !input.signals.find((signal) => {
-      return (p === signal.sY && goal === signal.sX) || (p === signal.bY && goal === signal.bX)
-    })
-  })
-  console.timeEnd("sol2")
-  return positions.length
-}
-const getPositions =( input: Input, goal: number): number[] => {
+const getPositions =(input: Input, goal: number): number[] => {
   let positions = [];
-  input.signals.forEach((signal) => {
+  input.forEach((signal) => {
     let maxSize = (signal.mDistance * 2) + 1;
     let dist = Math.abs(signal.sX - goal)
     let takenInRow = maxSize + (dist * -2);
@@ -122,6 +94,7 @@ const getPositions =( input: Input, goal: number): number[] => {
   return positions
 
 }
+
 const getDistance = (sX, sY, bX, bY): number => {
   return (Math.abs(sX - bX) + Math.abs(sY - bY));
 }
